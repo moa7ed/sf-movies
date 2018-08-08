80dc5c2f1a8becf354c1f3e50111284ed03670f2
moa7ed <moa7ed25@gmail.com>
2018-08-08 21:50:02 +0200

    remove turbolink gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 //= require underscore
-//= require gmaps/google
 //= require jquery.easy-autocomplete.min
+//= require gmaps/google
+//= require_tree .
--- deploy/before_restart.rb
+++
@@ -1,1 +0,0 @@
-run "cd /srv/www/sfmovies/current && /usr/local/bin/bundle exec rake assets:precompile"
